cc963bf320daa3aed0f7b8634ecb789b75e1ab66
roy.guo <roy.guo@coupang.com>
2019-06-21 18:07:29 +0800

    add logger

--- match-base/src/main/java/com/roy/football/match/MatchConfiguration.java
+++ match-base/src/main/java/com/roy/football/match/MatchConfiguration.java
@@ -1,7 +1,6 @@
 package com.roy.football.match;
 
 import java.util.concurrent.ExecutorService;
-import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.ThreadPoolExecutor;
 import java.util.concurrent.TimeUnit;
